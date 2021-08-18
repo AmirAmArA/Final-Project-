f536d946e77a2f93c5d93727fb77b0898de356c6
KaramAbuMokh <abu.mokh.karam@gmail.com>
2021-08-19 01:17:27 +0300

    finished line topology functionality

--- src/Components/Ring.js
+++ src/Components/Ring.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import Vertex from "../Structures/Vertex";
 import Edge from "../Structures/Edge";
 import Lightpath from "../Structures/Lightpath";
-import { rand, randN, createLightpaths } from '../Structures/helpFunc.js';
-import { getSVG, f1, f, shuffle, appear, wait } from './Circles'
-import d3 from 'd3'
-import '../App.css';
+import { rand, randN, createLightpaths } from "../Structures/helpFunc.js";
+import { getSVG, f1, f, shuffle, appear, wait } from "./Circles";
+import d3 from "d3";
+import "../App.css";
 
 export default class Ring extends Component {
   constructor() {
@@ -22,7 +22,6 @@ export default class Ring extends Component {
       showOnline: false,
       lpOnlineCNT: 0,
       showLpOnlineCNT: false,
-
     };
   }
 
@@ -55,59 +54,61 @@ export default class Ring extends Component {
   };
 
   produceLightpathsOptimal = () => {
-    const { vertexCount, vertexArr, edgeArr, lightpathArr, LParr, lpOnlineCNT } = this.state;
+    const {
+      vertexCount,
+      vertexArr,
+      edgeArr,
+      lightpathArr,
+      LParr,
+      lpOnlineCNT,
+    } = this.state;
     const optimalCirclesNum = rand(2, 7);
 
     let optimalCirclesArr = new Array(optimalCirclesNum);
 
-
     for (let i = 0; i < optimalCirclesNum; i++) {
-      optimalCirclesArr[i] = randN(vertexArr)
-
+      optimalCirclesArr[i] = randN(vertexArr);
     }
 
-    lightpathArr.push(...createLightpaths(optimalCirclesArr, vertexArr))
+    lightpathArr.push(...createLightpaths(optimalCirclesArr, vertexArr));
     console.log(lightpathArr);
 
-    optimalCirclesArr.unshift(vertexArr)
+    optimalCirclesArr.unshift(vertexArr);
 
-    getSVG(optimalCirclesArr, vertexArr.length)
-    LParr.push(...shuffle(lightpathArr))
+    getSVG(optimalCirclesArr, vertexArr.length);
+    LParr.push(...shuffle(lightpathArr));
     console.log(LParr);
-    this.setState({ lpOnlineCNT: lpOnlineCNT + f1([vertexArr], LParr, vertexArr.length, edgeArr.length) })
-  }
+    this.setState({
+      lpOnlineCNT:
+        lpOnlineCNT + f1([vertexArr], LParr, vertexArr.length, edgeArr.length),
+    });
+  };
 
   appear = () => {
     const { LParr, lpCNT } = this.state;
     console.log(lpCNT);
-    let paths = document.querySelectorAll(`.p${lpCNT}`)
+    let paths = document.querySelectorAll(`.p${lpCNT}`);
     if (lpCNT >= LParr.length) {
-
-      this.setState({ showLpOnlineCNT: true })
+      this.setState({ showLpOnlineCNT: true });
     } else {
-
-
       for (var j = 0; j < LParr[lpCNT].passing_edges.length; j++) {
-        paths[j].removeAttribute('display')
+        paths[j].removeAttribute("display");
       }
     }
-    paths = document.querySelectorAll(`.p${lpCNT}999`)
+    paths = document.querySelectorAll(`.p${lpCNT}999`);
 
     if (lpCNT >= LParr.length) {
-
-      this.setState({ showLpOnlineCNT: true })
+      this.setState({ showLpOnlineCNT: true });
     } else {
-
-
       for (var j = 0; j < paths.length; j++) {
-        paths[j].removeAttribute('display')
+        paths[j].removeAttribute("display");
       }
     }
-    this.setState({ lpCNT: this.state.lpCNT + 1 })
-  }
+    this.setState({ lpCNT: this.state.lpCNT + 1 });
+  };
 
   simulate = () => {
-    this.setState({ showOffline: true, showOnline: true })
+    this.setState({ showOffline: true, showOnline: true });
     this.produceGraph();
     this.produceLightpathsOptimal();
   };
@@ -124,63 +125,91 @@ export default class Ring extends Component {
       showOffline: false,
       showOnline: false,
       vertexCount: 0,
-      showLpOnlineCNT: false
-    })
-    document.querySelector('.svgpainter').innerHTML = '';
-    document.querySelector('.svgpainter3').innerHTML = '';
-  }
+      showLpOnlineCNT: false,
+    });
+    document.querySelector(".svgpainter").innerHTML = "";
+    document.querySelector(".svgpainter3").innerHTML = "";
+  };
 
   render() {
     return (
       <div className="container">
-        <h1 className="f1">How Many Nodes would you like to test the algorithm on? (Ring)</h1>
+        <h1>How Many Nodes would you like to test the algorithm on? (Ring)</h1>
         <div className="tc">
           <input
             type="text"
             placeholder="Number of Nodes"
             width="40%"
-            style={{ borderRadius: '10px' }}
-            onBlur={this.loadState}
+            style={{ borderRadius: "10px" }}
+            onChange={this.loadState}
             className="pa1 ma2 ba b--light-blue "
           />
 
-
           <div>
-            <Link to="/" className="generalButton">  Go Home </Link>
+            <Link to="/" className="generalButton">
+              {" "}
+              Go Home{" "}
+            </Link>
             <button className="generalButton" onClick={this.simulate}>
               Simulate
             </button>
-            <button className="generalButton" onClick={this.appear}>Step Over</button>
-            <button className="generalButton" onClick={this.reset}>Reset</button>
+            <button className="generalButton" onClick={this.appear}>
+              Step Over
+            </button>
+            <button className="generalButton" onClick={this.reset}>
+              Reset
+            </button>
           </div>
         </div>
+
         <div className="tc">
-          {this.state.showLpOnlineCNT ?
+          {this.state.showLpOnlineCNT ? (
             <h3>
-              The C-Ratio in this Simulation is : {this.state.lpOnlineCNT / this.state.LParr.length}</h3> : null}
+              The C-Ratio in this Simulation is :{" "}
+              {(this.state.lpOnlineCNT / this.state.LParr.length).toFixed(2)}
+            </h3>
+          ) : null}
         </div>
         <div className="container2">
-          <div className='tc ' style={{ display: this.state.showOffline ? null : 'none' }}>
-            <h3>Optimal Solution <br /> Total ADMs: {this.state.LParr.length}</h3>
-            <div className='tc svgpainter ' ></div>
-
+          <div
+            className="tc "
+            style={{ display: this.state.showOffline ? null : "none" }}
+          >
+            <h3>
+              Optimal Solution <br /> Total ADMs: {this.state.LParr.length}
+            </h3>
+            <div className="tc svgpainter "></div>
           </div>
 
-          <div className='tc ' style={{ display: this.state.showOnline ? null : 'none' }}>
-
+          <div
+            className="tc "
+            style={{ display: this.state.showOnline ? null : "none" }}
+          >
             <h3>
-              OnLine minADM Solution <br />Total ADMs:
+              OnLine minADM Solution <br />
+              Total ADMs:
               {this.state.showLpOnlineCNT ? this.state.lpOnlineCNT : null}
             </h3>
             <h4>
-              {this.state.showOnline ? this.state.LParr[this.state.lpCNT] != undefined ? 'Next Lightpath start is : ' + this.state.LParr[this.state.lpCNT].startVertex : null : null}  <br /> {this.state.showOnline ? this.state.LParr[this.state.lpCNT] != undefined ? 'end is : ' + this.state.LParr[this.state.lpCNT].endVertex : null : null}
+              {this.state.showOnline
+                ? this.state.LParr[this.state.lpCNT] != undefined
+                  ? "Next Lightpath start is : " +
+                    this.state.LParr[this.state.lpCNT].startVertex
+                  : null
+                : null}{" "}
+              <br />{" "}
+              {this.state.showOnline
+                ? this.state.LParr[this.state.lpCNT] != undefined
+                  ? "end is : " + this.state.LParr[this.state.lpCNT].endVertex
+                  : null
+                : null}
             </h4>
-            <div className='tc svgpainter3' style={{ transform: 'all 1s ease-in-out' }}></div>
+            <div
+              className="tc svgpainter3"
+              style={{ transform: "all 1s ease-in-out" }}
+            ></div>
           </div>
-
         </div>
-
-
       </div>
     );
   }
