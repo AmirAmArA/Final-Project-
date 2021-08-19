68a160e73bfc63a0c6cdeae526d8af4280ce2b08
AmirAmArA <amiramara11@gmail.com>
2021-08-19 03:15:09 +0300

    test

--- src/Router.js
+++ src/Router.js
@@ -3,15 +3,19 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 import Ring from "./Components/Ring";
 import Line from "./Components/Line";
 import Home from "./Components/Home";
+import Header from "./Components/Header";
 
 const Router = () => (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <div>
+      {/* <Header /> */}
       <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/Ring/" component={Ring} />
-        <Route path="/Line/" component={Line} />
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/Ring" component={Ring} />
+        <Route path="/Line" component={Line} />
+        {/* <Route path="/add-role" component={AddRole} /> */}
       </Switch>
+
     </div>
   </HashRouter>
 );
