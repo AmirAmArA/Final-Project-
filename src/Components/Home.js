9ae0473b426dd8d9bfec95a2e2cf8721c36775e8
AmirAmArA <amiramara11@gmail.com>
2021-08-19 03:29:31 +0300

    test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,6 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container">
-        <h1>lolololo</h1>
         <h1>
           Today we are going to present to you the span MinADM Algorithm and
           simulate it!
